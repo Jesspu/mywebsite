3dfcbed49518ac13315eaf3ab07906cb39dcb8e7
Jesspu <Jesspu44@gmail.com>
2015-08-21 15:09:37 -0500

    fix2

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ var app = express()
 var port = process.env.PORT || 8080;
 
 var router = express.Router()
-var angular = express.Router()
+
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
-router.get('/angularProjects', function (req, res) {
-    res.sendFile(path.join)(__dirname + '/angular/index.html')
-})
+
 app.use('/', router)
-app.use('/angularProjects', angular)
-app.use(express.static('/angular/public'))
+
 app.use(express.static('assets'))
 
 app.listen(port)
