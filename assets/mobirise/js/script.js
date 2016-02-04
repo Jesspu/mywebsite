b593cdad25212e1b0f3f8ccf2ba4f0f2466d9606
Jess Patton <Jesspu44@gmail.com>
2016-02-04 18:41:20 -0500

    -update
    updated files

--- assets/mobirise/js/script.js
+++ assets/mobirise/js/script.js
@@ -11,6 +11,30 @@
         return this.find(selector).addBack(selector);
     };
 
+    (function($,sr){
+        // debouncing function from John Hann
+        // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+        var debounce = function (func, threshold, execAsap) {
+            var timeout;
+
+            return function debounced () {
+                var obj = this, args = arguments;
+                function delayed () {
+                    if (!execAsap) func.apply(obj, args);
+                    timeout = null;
+                };
+
+                if (timeout) clearTimeout(timeout);
+                else if (execAsap) func.apply(obj, args);
+
+                timeout = setTimeout(delayed, threshold || 100);
+            };
+        }
+        // smartresize 
+        jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+    })(jQuery,'smartresize');
+
     (function(){
         
         var scrollbarWidth = 0, originalMargin, touchHandler = function(event){
@@ -35,33 +59,6 @@
             return scrollbarWidth;
         }
 
-        $.fn.fullscreen = function(yes){
-            if (yes){
-                originalMargin = document.body.parentNode.style.marginRight || '';
-                var fullWindowWidth = window.innerWidth;
-                if (!fullWindowWidth){
-                    var documentElementRect = document.documentElement.getBoundingClientRect();
-                    fullWindowWidth = documentElementRect.right - Math.abs(documentElementRect.left);
-                }
-                if (fullWindowWidth > document.body.clientWidth){
-                    $('html').css({
-                        'margin-right' : parseInt(($('html').css('margin-right') || 0), 10) + getScrollbarWidth(),
-                        'overflow' : 'hidden'
-                    }).addClass('mbr-hidden-scrollbar');
-                }
-                this.addClass('mbr-fullscreen');
-                $(window).bind('touchmove', touchHandler).resize();
-            } else {
-                this.removeClass('mbr-fullscreen').css('height', '');
-                $('html').css({
-                    'margin-right' : originalMargin,
-                    'overflow' : ''
-                }).removeClass('mbr-hidden-scrollbar');
-                $(window).unbind('touchmove', touchHandler);
-            }
-            return this;
-        };
-
     })();
 
     $.isMobile = function(type){
@@ -79,36 +76,66 @@
         return !!(reg.length && navigator.userAgent.match(new RegExp(reg.join('|'), 'i')));
     };
 
+    var isSupportViewportUnits = (function(){
+        // modernizr implementation
+        var $elem = $('<div style="height: 50vh; position: absolute; top: -1000px; left: -1000px;">').appendTo('body');
+        var elem = $elem[0];
+        var height = parseInt(window.innerHeight / 2, 10);
+        var compStyle = parseInt((window.getComputedStyle ? getComputedStyle(elem, null) : elem.currentStyle)['height'], 10);
+        $elem.remove();
+        return compStyle == height;
+    }());
+
     $(function(){
 
         $('html').addClass($.isMobile() ? 'mobile' : 'desktop');
 
-        // .mbr-fullscreen
-        (function(width, height){
-            var deviceSize = [width, width];
-            deviceSize[height > width ? 0 : 1] = height;
-            $(window).resize(function(){
-                var windowHeight = $(window).height();
-                // simple fix for Chrome's scrolling
-                if ($.isMobile() && navigator.userAgent.match(/Chrome/i) && $.inArray(windowHeight, deviceSize) < 0)
-                    windowHeight = deviceSize[ $(window).width() > windowHeight ? 1 : 0 ];
-                $('.mbr-fullscreen').each(function(){
-                    $(this).css('height', windowHeight + 'px');
-                });
+        // .mbr-navbar--sticky
+        $(window).scroll(function(){
+            $('.mbr-navbar--sticky').each(function(){
+                var method = $(window).scrollTop() > 10 ? 'addClass' : 'removeClass';
+                $(this)[method]('mbr-navbar--stuck')
+                    .not('.mbr-navbar--open')[method]('mbr-navbar--short');
             });
-        })($(window).width(), $(window).height());
-        $(document).on('add.cards', function(event){
-            if ($('html').hasClass('mbr-site-loaded') && $(event.target).outerFind('.mbr-fullscreen').length)
-                $(window).resize();
         });
 
-        // fallback for .mbr-section--full-height
-        if (navigator.userAgent.match(/MSIE (8|9|10)\./) || $.isMobile('iOS')){
-            $(window).resize(function(){
-                var windowHeight = $(window).height();
-                $('.mbr-section--full-height').each(function(){
-                    $(this).css('height', windowHeight + 'px');
+        // .mbr-hamburger
+        $(document).on('add.cards change.cards', function(event){
+            $(event.target).outerFind('.mbr-hamburger:not(.mbr-added)').each(function(){
+                $(this).addClass('mbr-added')
+                    .click(function(){
+                        $(this)
+                            .toggleClass('mbr-hamburger--open')
+                            .parents('.mbr-navbar')
+                            .toggleClass('mbr-navbar--open')
+                            .removeClass('mbr-navbar--short');
+                    }).parents('.mbr-navbar').find('a:not(.mbr-hamburger)').click(function(){
+                        $('.mbr-hamburger--open').click();
+                    });
+            });
+        });
+        $(window).smartresize(function(){
+            if ($(window).width() > 991)
+                $('.mbr-navbar--auto-collapse .mbr-hamburger--open').click();
+        }).keydown(function(event){
+            if (27 == event.which) // ESC
+                $('.mbr-hamburger--open').click();
+        });
+
+        if ($.isMobile() && navigator.userAgent.match(/Chrome/i)){ // simple fix for Chrome's scrolling
+            (function(width, height){
+                var deviceSize = [width, width];
+                deviceSize[height > width ? 0 : 1] = height;
+                $(window).smartresize(function(){
+                    var windowHeight = $(window).height();
+                    if ($.inArray(windowHeight, deviceSize) < 0)
+                        windowHeight = deviceSize[ $(window).width() > windowHeight ? 1 : 0 ];
+                    $('.mbr-section--full-height').css('height', windowHeight + 'px');
                 });
+            })($(window).width(), $(window).height());
+        } else if (!isSupportViewportUnits){ // fallback for .mbr-section--full-height
+            $(window).smartresize(function(){
+                $('.mbr-section--full-height').css('height', $(window).height() + 'px');
             });
             $(document).on('add.cards', function(event){
                 if ($('html').hasClass('mbr-site-loaded') && $(event.target).outerFind('.mbr-section--full-height').length)
@@ -116,66 +143,49 @@
             });
         }
 
-        // .mbr-parallax-background
-        $(window).on('message', function(event){
-            if (typeof $.fn.parallax !== 'undefined' && 'destroy.parallax' === event.originalEvent.data.type){
-                $('[data-parallax-id="' + event.originalEvent.data.id + '"]')
-                    .removeClass('mbr-added')
-                    .parallax('destroy');
-            }
+        // .mbr-section--16by9 (16 by 9 blocks autoheight)
+        function calculate16by9(){
+            $(this).css('height', $(this).parent().width() * 9 / 16);
+        }
+        $(window).smartresize(function(){
+            $('.mbr-section--16by9').each(calculate16by9);
         });
         $(document).on('add.cards change.cards', function(event){
-            if(typeof $.fn.parallax !== 'undefined') {
-                $(event.target).outerFind('.mbr-parallax-background:not(.mbr-added)').each(function(){
-                    $(this).addClass('mbr-added');
-                    if (!$.isMobile()){
-                        $(this).attr('data-parallax-id', ('' + Math.random()).replace(/\D/g, '')).parallax('50%', 0.3, true);
-                    }
-                });
+            var enabled = $(event.target).outerFind('.mbr-section--16by9');
+            if (enabled.length){
+                enabled
+                    .attr('data-16by9', 'true')
+                    .each(calculate16by9);
+            } else {
+                $(event.target).outerFind('[data-16by9]')
+                    .css('height', '')
+                    .removeAttr('data-16by9');
             }
         });
 
+
+        // .mbr-parallax-background
+        if ($.fn.jarallax && !$.isMobile()){
+            $(document).on('destroy.parallax', function(event){
+                $(event.target).outerFind('.mbr-parallax-background')
+                    .jarallax('destroy')
+                    .css('position', '');
+            });
+            $(document).on('add.cards change.cards', function(event){
+                $(event.target).outerFind('.mbr-parallax-background')
+                    .jarallax()
+                    .css('position', 'relative');
+            });
+        }
+
         // .mbr-social-likes
-        $(document).on('add.cards', function(event){
-            if(typeof $.fn.socialLikes !== 'undefined') {
+        if ($.fn.socialLikes){
+            $(document).on('add.cards', function(event){
                 $(event.target).outerFind('.mbr-social-likes:not(.mbr-added)').on('counter.social-likes', function(event, service, counter){
                     if (counter > 999) $('.social-likes__counter', event.target).html(Math.floor(counter / 1000) + 'k');
                 }).socialLikes({initHtml : false});
-            }
-        });
-
-        // .mbr-nav-collapse, .mbr-nav-toggle
-        var autoCollapse = function(area){
-            if ($(window).width() > 780){
-                area.outerFind('.mbr-nav-collapse:not(.collapsed)').removeClass('nav-collapsed mbr-nav-visible')
-                    .find('.mbr-nav-toggle.opened').click();
-            } else {
-                area.outerFind('.mbr-nav-collapse').addClass('nav-collapsed')
-                    .find('.mbr-nav-toggle').show();
-            }
-        };
-        $(window).resize(function(){
-            autoCollapse($('body'));
-        }).keydown(function(event){
-            if (27 == event.which) // ESC
-                $('.mbr-nav-toggle.opened').click();
-        });
-        $(document).on('add.cards', function(event){
-            $('.mbr-nav-toggle:not(.mbr-added)', event.target).addClass('mbr-added').click(function(){
-                var parent = $(this).parents('[class|="menu"]');
-                var open = !$(this).hasClass('opened');
-                $('nav', parent).fullscreen(open);
-                $(this)[ (open ? 'add' : 'remove') + 'Class' ]('opened');
-                parent[ (open ? 'add' : 'remove') + 'Class' ]('mbr-nav-visible')
-                    .css('top', open ? $(window).scrollTop() : '');
-            }).parents('[class|="menu"]').find('nav a').click(function(){
-                $('.mbr-nav-toggle.opened').click();
             });
-        });
-        $(document).on('change.cards', function(event){
-            if ($(event.target).outerFind('.mbr-nav-collapse').length)
-                autoCollapse($(event.target));
-        });
+        }
 
         // .mbr-fixed-top
         var fixedTopTimeout, scrollTimeout, prevScrollTop = 0, fixedTop = null, isDesktop = !$.isMobile();
@@ -247,12 +257,14 @@
             }).end().html('').addClass('mbr-google-map--loaded');
             if (markers.length){
                 var map = this.Map = new google.maps.Map(this, {
-                    scrollwheel: false,
-                    zoom      : params.zoom,
-                    mapTypeId : google.maps.MapTypeId[params.type],
-                    center    : coord(params.center || markers[0].coord)
+                    scrollwheel : false,
+                    // prevent draggable on mobile devices
+                    draggable   : !$.isMobile(),
+                    zoom        : params.zoom,
+                    mapTypeId   : google.maps.MapTypeId[params.type],
+                    center      : coord(params.center || markers[0].coord)
                 });
-                $(window).resize(function(){
+                $(window).smartresize(function(){
                    var center = map.getCenter();
                    google.maps.event.trigger(map, 'resize');
                    map.setCenter(center); 
@@ -296,7 +308,7 @@
         });
 
         // embedded videos
-        $(window).resize(function(){
+        $(window).smartresize(function(){
             $('.mbr-embedded-video').each(function(){
                 $(this).height(
                     $(this).width() *
@@ -310,37 +322,6 @@
                 $(window).resize();
         });
 
-        // background video
-        var updateBgImgPosition = function(img){
-            var win = {
-                width : img.parent().outerWidth(),
-                height : img.parent().outerHeight()
-            };
-            var ratio = '16/9';
-            var margin = 24;
-            var overprint = 100;
-            var css = {};
-            css.width = win.width + ((win.width * margin) / 100);
-            css.height = ratio == '16/9' ? Math.ceil((9 * win.width) / 16) : Math.ceil((3 * win.width) / 4);
-            css.marginTop = -((css.height - win.height) / 2);
-            css.marginLeft = -((win.width * (margin / 2)) / 100);
-            if (css.height < win.height){
-                css.height = win.height + ((win.height * margin) / 100);
-                css.width = ratio == '16/9' ? Math.floor((16 * win.height) / 9) : Math.floor((4 * win.height) / 3);
-                css.marginTop = -((win.height * (margin / 2)) / 100);
-                css.marginLeft = -((css.width - win.width) / 2);
-            }
-            css.width += overprint;
-            css.height += overprint;
-            css.marginTop -= overprint / 2;
-            css.marginLeft -= overprint / 2;
-            img.css(css);
-        };
-        $(window).resize(function(){
-            $('.mbr-background-video-preview img').each(function(){
-                updateBgImgPosition($(this));
-            });
-        });
         $(document).on('add.cards', function(event){
             $(event.target).outerFind('[data-bg-video]').each(function(){
                 var result, videoURL = $(this).data('bg-video'), patterns = [
@@ -352,30 +333,34 @@
                     if (result = patterns[i].exec(videoURL)){
                         var previewURL = 'http' + ('https:' == location.protocol ? 's' : '') + ':';
                         previewURL += '//img.youtube.com/vi/' + result[1] + '/maxresdefault.jpg';
-                        $('.container:eq(0)', this).before(
-                            $('<img>')
-                                .hide()
-                                .wrap('<div class="mbr-background-video-preview"></div>')
-                                .on('load', function(){
-                                    if (120 == (this.naturalWidth || this.width)){
 
-                                        // selection of preview in the best quality
-                                        var file = this.src.split('/').pop();
-                                        switch (file){
-                                            case 'maxresdefault.jpg':
-                                                this.src = this.src.replace(file, 'sddefault.jpg');
-                                                break;
-                                            case 'sddefault.jpg':
-                                                this.src = this.src.replace(file, 'hqdefault.jpg');
-                                                break;
-                                        }
-                                        
-                                    } else $(this).show();
-                                })
-                                .attr('src', previewURL)
-                                .parent()
-                        );
-                        if (typeof $.fn.YTPlayer !== 'undefined' && !$.isMobile()){
+                        var $img = $('<div class="mbr-background-video-preview">')
+                            .hide()
+                            .css({
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center'
+                            })
+                        $('.container:eq(0)', this).before($img);
+
+                        $('<img>').on('load', function() {
+                            if (120 == (this.naturalWidth || this.width)) {
+                                // selection of preview in the best quality
+                                var file = this.src.split('/').pop();
+                                switch (file){
+                                    case 'maxresdefault.jpg':
+                                        this.src = this.src.replace(file, 'sddefault.jpg');
+                                        break;
+                                    case 'sddefault.jpg':
+                                        this.src = this.src.replace(file, 'hqdefault.jpg');
+                                        break;
+                                }
+                            } else {
+                                $img.css('background-image', 'url("' + this.src + '")')
+                                    .show();
+                            }
+                        }).attr('src', previewURL)
+
+                        if ($.fn.YTPlayer && !$.isMobile()){
                             var params = eval('(' + ($(this).data('bg-video-params') || '{}') + ')');
                             $('.container:eq(0)', this).before('<div class="mbr-background-video"></div>').prev()
                                 .YTPlayer($.extend({
@@ -406,9 +391,11 @@
                             var useBody = /#bottom|#top/g.test(target.hash);
                             $(useBody ? 'body' : target.hash).each(function(){
                                 e.preventDefault();
+                                // in css sticky navbar has height 64px 
+                                var stickyMenuHeight = $('.mbr-navbar--sticky').length ? 64 : 0;
                                 var goTo = target.hash == '#bottom' 
                                         ? ($(this).height() - $(window).height())
-                                        : $(this).offset().top;
+                                        : ($(this).offset().top - stickyMenuHeight);
                                 $('html, body').stop().animate({
                                     scrollTop: goTo
                                 }, 800, 'easeInOutCubic');
@@ -432,7 +419,7 @@
         var e = document.createElement("section");
         e.id = "top-1";
         e.className = "engine";
-        e.innerHTML = '<a href="http://mobirise.com">mobirise.com</a> Mobirise v1.9.2';
+        e.innerHTML = '<a href="https://mobirise.com">mobirise.com</a> Mobirise v2.8.6';
         document.body.insertBefore(e, document.body.childNodes[0]);
     }
 }();
